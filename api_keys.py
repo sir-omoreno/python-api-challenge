50952b8ae849e680d95179a1a88cc175b53709bc
sir-omoreno <ozmoreno@gmail.com>
2021-03-11 23:24:57 -0500

    Done, cleaned api keys

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "38d8f2309db8ab6f5de0214380a70934"
+weather_api_key = "YOUR KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyDCt9hDWUt36T3ajlAim6WFReom60M2y1A"
+g_key = "YOUR KEY HERE"
